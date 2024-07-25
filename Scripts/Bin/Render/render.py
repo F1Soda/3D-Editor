4e2884c41b451886bf9a200a0934cc509356194c
Тимогей <golik.tima@gmail.com>
2024-07-25 21:34:07 +0500

    Убрал ненужное

--- Scripts/Bin/Render/render.py
+++
@@ -1,7 +0,0 @@
-import Scripts.Bin.Render.mesh as mesh_m
-import Scripts.Bin.Render.material as material_m
-import Scripts.Bin.Render.shaderprogram as shader_program_m
-
-Mesh = mesh_m.Mesh
-Material = material_m.Material
-ShaderProgram = shader_program_m.ShaderProgram
